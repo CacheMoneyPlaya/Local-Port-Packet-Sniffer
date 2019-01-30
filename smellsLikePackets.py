e814df53cc475b6c85b6fa4f0564e05609cefab9
Jan van Bergen <39480577+ThePeelerShredder@users.noreply.github.com>
2019-01-30 20:44:51 +0000

    Update smellsLikePackets.py

--- smellsLikePackets.py
+++ smellsLikePackets.py
@@ -1,23 +1,24 @@
 import socket
-import struct
 import binascii
+import struct
 import textwrap
 
 def main():
-    # Get host
+    #Initialization
     host = socket.gethostbyname(socket.gethostname())
     print('IP: {}'.format(host))
 
-    # Create a raw socket and bind it
+    
     conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
     conn.bind((host, 0))
 
-    # Include IP headers
+   
     conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-    # Enable promiscuous mode
+    
     conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
+    #Loop listening for packets
     while True:
+        
         # Recive data
         raw_data, addr = conn.recvfrom(65536)
 
@@ -29,18 +30,16 @@ def main():
         print("Source MAC: {}".format(src_mac))
         print("Protocol: {}".format(eth_proto))
 
-# Unpack ethernet frame
+    
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('!6s6s2s', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), get_protocol(proto), data[14:]
 
-# Return formatted MAC address AA:BB:CC:DD:EE:FF
 def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     mac_address = ':'.join(bytes_str).upper()
     return mac_address
 
-# Return formatted protocol ABCD
 def get_protocol(bytes_proto):
     bytes_str = map('{:02x}'.format, bytes_proto)
     protocol = ''.join(bytes_str).upper()
